f8c2325accc9222e01e3381086fde466612c8edb
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2020-08-01 13:42:49 +0700

    ✨ Add website

--- index.js
+++ index.js
@@ -1,51 +1,66 @@
-const { argv } = process
-const [loan, mrr, startPayment] = argv.slice(2)
+;(function (window) {
+  window.calculateMortgage = calculateMortgage
 
-const MRR = Number(mrr) / 100
-const NUMBER_OF_MONTHS_IN_YEAR = 12
-const NUMBER_OF_INSTALLMENTS = 30 * NUMBER_OF_MONTHS_IN_YEAR
-const MARGIN_END_BALANCE = 0
-const PAYMENT_STEP = 10
+  function calculateMortgage(loan, mrr, startPayment) {
+    // const { argv } = process
+    // const [loan, mrr, startPayment] = argv.slice(2)
 
-let payment = Number(startPayment) || 30000
-let accumulatedBalance = Number(loan)
-let totalInterest = 0
-// 1st round
-calculate()
+    const MRR = Number(mrr) / 100
+    const NUMBER_OF_MONTHS_IN_YEAR = 12
+    const NUMBER_OF_INSTALLMENTS = 30 * NUMBER_OF_MONTHS_IN_YEAR
+    const MARGIN_END_BALANCE = 0
+    const PAYMENT_STEP = 10
 
-// consecutive rounds
-while (accumulatedBalance > MARGIN_END_BALANCE) {
-  payment += PAYMENT_STEP
-  accumulatedBalance = Number(loan)
-  totalInterest = 0
+    let payment = Number(startPayment) || 30000
+    let accumulatedBalance = Number(loan)
+    let totalInterest = 0
+    // 1st round
+    loop()
 
-  calculate()
-}
+    // consecutive rounds
+    while (accumulatedBalance > MARGIN_END_BALANCE) {
+      payment += PAYMENT_STEP
+      accumulatedBalance = Number(loan)
+      totalInterest = 0
 
-function calculate() {
-  for (let i = 0; i < NUMBER_OF_INSTALLMENTS; i++) {
-    const interest = (MRR * accumulatedBalance) / NUMBER_OF_MONTHS_IN_YEAR
-    totalInterest += interest
-    accumulatedBalance = accumulatedBalance - (payment - interest)
+      loop()
+    }
+
+    function loop() {
+      for (let i = 0; i < NUMBER_OF_INSTALLMENTS; i++) {
+        const interest = (MRR * accumulatedBalance) / NUMBER_OF_MONTHS_IN_YEAR
+        totalInterest += interest
+        accumulatedBalance = accumulatedBalance - (payment - interest)
+      }
+    }
+
+    const data = {
+      numberOfInstallments: NUMBER_OF_INSTALLMENTS,
+      payment,
+      totalInterests: formatCurrency(totalInterest),
+      interestByLoan: formatNumber((totalInterest / Number(loan)) * 100) + "%",
+      totalPayment: formatCurrency(
+        payment * NUMBER_OF_INSTALLMENTS + accumulatedBalance
+      ),
+    }
+
+    console.log("No. of installments:", data.numberOfInstallments)
+    console.log("Payment per installment:", data.payment)
+    console.log("Total interest", data.totalInterests)
+    console.log("Total interest (% of loan)", data.interestByLoan)
+    console.log("Total payment", data.totalPayment)
+
+    return data
   }
-}
 
-console.log("No. of installments:", NUMBER_OF_INSTALLMENTS)
-console.log("Payment per installment:", payment)
-console.log("Total interest", formatCurrency(totalInterest))
-console.log(
-  "Total interest (% of loan)",
-  formatNumber((totalInterest / Number(loan)) * 100) + "%"
-)
-console.log("Total payment", formatCurrency(payment * NUMBER_OF_INSTALLMENTS))
+  function formatNumber(number) {
+    return new Intl.NumberFormat("th-TH").format(number)
+  }
 
-function formatNumber(number) {
-  return new Intl.NumberFormat("th-TH").format(number)
-}
-
-function formatCurrency(number) {
-  return new Intl.NumberFormat("th-TH", {
-    style: "currency",
-    currency: "THB",
-  }).format(number)
-}
+  function formatCurrency(number) {
+    return new Intl.NumberFormat("th-TH", {
+      style: "currency",
+      currency: "THB",
+    }).format(number)
+  }
+})(window)
